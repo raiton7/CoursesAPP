9c581ebe293ae7f686b07eb38189d5ac55e3c21e
Grzegorz Jurek <gjurek777@gmail.com>
2021-07-27 12:44:08 +0200

    Add Courses and Topics controllers

---
+++ CoursesAPI/Controllers/CoursesController.cs
@@ -0,0 +1,105 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using CoursesAPI.Models;
+
+namespace CoursesAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CoursesController : ControllerBase
+    {
+        private readonly CoursesContext _context;
+
+        public CoursesController(CoursesContext context)
+        {
+            _context = context;
+        }
+
+        // GET: api/Courses
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<Course>>> GetList()
+        {
+            return await _context.Courses.ToListAsync();
+        }
+
+        // GET: api/Courses/5
+        [HttpGet("{id}")]
+        public async Task<ActionResult<Course>> GetCourse(long id)
+        {
+            var course = await _context.Courses.FindAsync(id);
+
+            if (course == null)
+            {
+                return NotFound();
+            }
+
+            return course;
+        }
+
+        // PUT: api/Courses/5
+        [HttpPut("{id}")]
+        public async Task<IActionResult> EditCourse(long id, Course course)
+        {
+            if (id != course.Id)
+            {
+                return BadRequest();
+            }
+
+            _context.Entry(course).State = EntityState.Modified;
+
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!CourseExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return NoContent();
+        }
+
+        // POST: api/Courses
+        [HttpPost]
+        public async Task<ActionResult<Course>> CreateCourse(Course course)
+        {
+            _context.Courses.Add(course);
+            await _context.SaveChangesAsync();
+
+            return CreatedAtAction("GetCourse", new { id = course.Id }, course);
+        }
+
+        // DELETE: api/Courses/5
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeleteCourse(long id)
+        {
+            var course = await _context.Courses.FindAsync(id);
+            if (course == null)
+            {
+                return NotFound();
+            }
+
+            _context.Courses.Remove(course);
+            await _context.SaveChangesAsync();
+
+            return NoContent();
+        }
+
+        private bool CourseExists(long id)
+        {
+            return _context.Courses.Any(e => e.Id == id);
+        }
+    }
+}
---
+++ CoursesAPI/Controllers/TopicsController.cs
@@ -0,0 +1,105 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using CoursesAPI.Models;
+
+namespace CoursesAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class TopicsController : ControllerBase
+    {
+        private readonly CoursesContext _context;
+
+        public TopicsController(CoursesContext context)
+        {
+            _context = context;
+        }
+
+        // GET: api/Topics
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<Topic>>> GetList()
+        {
+            return await _context.Topics.ToListAsync();
+        }
+
+        // GET: api/Topics/5
+        [HttpGet("{id}")]
+        public async Task<ActionResult<Topic>> GetTopic(long id)
+        {
+            var topic = await _context.Topics.FindAsync(id);
+
+            if (topic == null)
+            {
+                return NotFound();
+            }
+
+            return topic;
+        }
+
+        // PUT: api/Topics/5
+        [HttpPut("{id}")]
+        public async Task<IActionResult> EditTopic(long id, Topic topic)
+        {
+            if (id != topic.Id)
+            {
+                return BadRequest();
+            }
+
+            _context.Entry(topic).State = EntityState.Modified;
+
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!TopicExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return NoContent();
+        }
+
+        // POST: api/Topics
+        [HttpPost]
+        public async Task<ActionResult<Topic>> CreateTopic(Topic topic)
+        {
+            _context.Topics.Add(topic);
+            await _context.SaveChangesAsync();
+
+            return CreatedAtAction("GetTopic", new { id = topic.Id }, topic);
+        }
+
+        // DELETE: api/Topics/5
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeleteTopic(long id)
+        {
+            var topic = await _context.Topics.FindAsync(id);
+            if (topic == null)
+            {
+                return NotFound();
+            }
+
+            _context.Topics.Remove(topic);
+            await _context.SaveChangesAsync();
+
+            return NoContent();
+        }
+
+        private bool TopicExists(long id)
+        {
+            return _context.Topics.Any(e => e.Id == id);
+        }
+    }
+}
