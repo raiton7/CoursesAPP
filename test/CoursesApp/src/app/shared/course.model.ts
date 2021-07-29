9d5392d28120e05a1e33ace00ab61f04c4d7380f
Grzegorz Jurek <gjurek777@gmail.com>
2021-07-29 21:55:59 +0200

    Removed unnecesary files

--- test/CoursesApp/src/app/shared/course.model.ts
+++
@@ -1,5 +0,0 @@
-export class Course {
-    id: number = 0;
-    name: string = '';
-    details: string = '';
-}
--- test/CoursesApp/src/app/shared/topic.model.ts
+++
@@ -1,6 +0,0 @@
-export class Topic {
-    id: number = 0;
-    name: string = '';
-    number: number = 0;
-    courseId: number = 0;
-}
